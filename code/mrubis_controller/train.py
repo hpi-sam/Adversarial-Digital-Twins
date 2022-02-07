c92382130ec12ae5efd29b9b7829fc48dfe09e1e
Philipp Hildebrandt <philipp.hildebrandt@student.hpi.uni-potsdam.de>
2022-02-07 16:05:14 +0100

    load model weights

--- code/mrubis_controller/train.py
+++ code/mrubis_controller/train.py
@@ -3,11 +3,17 @@ from agent.models.ranking_predictor import RankingPredictor
 from agent.agent import Agent
 from digital_twin.digital_twin import DigitalTwin, ShopDigitalTwin
 from trainer.trainer import Trainer
-
+import torch
+import numpy
+import random
 
 if __name__ == "__main__":
+    torch.manual_seed(0)
+    random.seed(0)
+    numpy.random.seed(0)
     fix_predictor = FixPredictor()
     ranking_predictor = RankingPredictor()
     agent = Agent(fix_predictor, ranking_predictor)
+    agent.fix_predictor.load_state_dict(torch.load('modelweights.pth'))
     trainer = Trainer(agent=agent, digital_twin=DigitalTwin())
-    trainer.train(max_runs=10000)
+    trainer.train(max_runs=1000)
